622d0d45a76f356e391636f1fc8ad0323bc3a641
Mxhrshad <mserattalab@gmail.com>
2024-07-13 18:56:33 +0330

    making the moving up logic in the jump function. making it more realistic by adding the gravity variable.

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const dino = document.querySelector(".dino");
 const grid = document.querySelector(".grid");
+let gravity = 0.9;
 
 function control(e){
-    if (e.code == 'space'){
-        jump()
+    if (e.code == 'Space'){
+        jump();
     };
 };
+
+let postition = 0
+function jump(){
+    let timerId = setInterval(() => {
+
+        // move up
+        postition += 30;
+        postition = postition * 0.9
+        dino.style.bottom = postition + 'px';
+
+    }, 20);
+
+}
 
 document.addEventListener('keydown', control);
